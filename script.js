3a0c006f14222529ea6dc90395188228480f020e
Abhinav Mathur <142848301+Abhinav290397@users.noreply.github.com>
2023-12-10 20:34:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function volume_sphere() {
     //Write your code here
-  
+	const Form = document.querySelector("#MyForm");
+	event.preventDefault();
+	const Rad = Form.radius.value;
+	const Vol = 1.33*Math.PI*Rad*Rad*Rad;
+    Form.volume.value = Vol;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
